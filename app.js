13373b48f16585c9d8c8bb0c43235b914994a327
1-Shreyash <108738197+1-Shreyash@users.noreply.github.com>
2023-05-16 13:35:50 +0530

    changes✨

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 btn = document.querySelectorAll(".keys");
 scr = document.querySelector(".screen")
-btn.forEach(element => {
-    element.onclick( () =>{
-        scr.value = element.innerHTML;
+btn.forEach((element,index) => {
+    if(index == 0){
+        element.addEventListener("click", ()=>{
+            let str = scr.value;
+        })
     }
-    )
+    else if(index == 2){
+        let str = scr.value
+        element.addEventListener("click", ()=>{
+            
+        })
+    }
+    else{
+        element.addEventListener("click", ()=>{
+            scr.value += element.innerHTML;
+        })
+    }
 });
+// btn[6].addEventListener("click")
